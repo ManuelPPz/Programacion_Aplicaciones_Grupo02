865334370ebfc52b71ffac52a5de49bead4745f2
manuelpalumbo <manuelpalumbo@localhost>
2026-08-13 12:11:53 -0300

    pantalla

--- src/main/java/com/mycompany/proyecto_proframacion_grupo02/Proyecto_Proframacion_grupo02.java
+++ src/main/java/com/mycompany/proyecto_proframacion_grupo02/Proyecto_Proframacion_grupo02.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.proyecto_proframacion_grupo02;
 
-/**
- *
- * @author manuelpalumbo
- */
+
+
+
 public class Proyecto_Proframacion_grupo02 {
 
-    public static void main(String[] args) {
-        System.out.println("Pruebaaxaaa!");
-        System.out.println("pruebbss");
-        System.out.print("en la pc de mate");
-    }
 }
