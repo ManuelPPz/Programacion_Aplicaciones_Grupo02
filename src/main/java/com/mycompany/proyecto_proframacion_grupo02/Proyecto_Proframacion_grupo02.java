cbaf92a8a47cae902111f0e5d150aeebedc8eee8
manuelpalumbo <manuelpalumbo@localhost>
2026-08-13 10:04:40 -0300

    Updated

---
+++ src/main/java/com/mycompany/proyecto_proframacion_grupo02/Proyecto_Proframacion_grupo02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_proframacion_grupo02;
+
+/**
+ *
+ * @author manuelpalumbo
+ */
+public class Proyecto_Proframacion_grupo02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
