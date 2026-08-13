2de90fc6f5d520465e8afcfcb32e205df4e9da3a
manuelpalumbo <manuelpalumbo@localhost>
2026-08-13 11:20:17 -0300

    Update

--- src/main/java/com/mycompany/proyecto_proframacion_grupo02/Proyecto_Proframacion_grupo02.java
+++ src/main/java/com/mycompany/proyecto_proframacion_grupo02/Proyecto_Proframacion_grupo02.java
@@ -11,6 +11,7 @@ package com.mycompany.proyecto_proframacion_grupo02;
 public class Proyecto_Proframacion_grupo02 {
 
     public static void main(String[] args) {
-        System.out.println("Prueba!");
+        System.out.println("Pruebaaxaaa!");
+        System.out.println("pruebbss");
     }
 }
