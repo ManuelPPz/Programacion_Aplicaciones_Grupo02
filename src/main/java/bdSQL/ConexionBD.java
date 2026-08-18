9e612ef1132c13959e7954fa0ade3e5a4fc21b08
manuelpalumbo <manuelpalumbo@localhost>
2026-08-17 23:38:42 -0300

    logia de la bd y creacion de tablas

---
+++ src/main/java/bdSQL/ConexionBD.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bdSQL;
+
+/**
+ *
+ * @author manuelpalumbo
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+    
+    private static final String URL = "jdbc:mysql//localhost:3306/edEXT_db?useSSl=false&serverTimezone=UTC";
+    //hay que cambiarlo y crear un perfil exclusivo para la bd actualmente tiene mis claves xD
+    private static final String USER = "root";
+    private static final String PASSWORD = "ghiuliana";
+    
+    public static Connection getConexion() throws SQLException {
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+                } catch (ClassNotFoundException e) {
+                throw new SQLException("No se encontro el driver de MySQL: " + e.getMessage());
+                }
+    return DriverManager.getConnection(URL, USER, PASSWORD);
+   }
+    
+ }
