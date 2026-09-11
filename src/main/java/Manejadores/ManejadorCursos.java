7a2c856bcab4a96d19069376f6be12b4e00e7c4c
manuelpalumbo <manuelpalumbo@localhost>
2026-09-11 10:16:20 -0300

    a

--- src/main/java/Manejadores/ManejadorCursos.java
+++ src/main/java/Manejadores/ManejadorCursos.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import Classes.Curso;
 import Classes.EdicionCurso; 
 import Classes.Instituto;
+import Classes.ProgramaDeFormacion;
 import Classes.UsuarioBase;
 import DTsClasses.DTCurso;
 import DTsClasses.DTMaster;
@@ -136,8 +137,15 @@ public class ManejadorCursos {
                 }
             }
         }
-
+        List<ProgramaDeFormacion> auxProgramas = c.getProgramas();
         List<String> auxProgramasStr = new ArrayList<>();
+        if (auxProgramasStr != null) {
+            for(ProgramaDeFormacion programa : auxProgramas){
+                if (programa != null) {
+                    auxProgramasStr.add(programa.getNombre());
+                }
+            }
+        }
 
         return new DTCurso(
             ins,
